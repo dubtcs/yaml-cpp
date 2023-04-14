3f8a8cda3eca484e815f368d1b0ecae570ca0555
dubtcs <75275524+dubtcs@users.noreply.github.com>
2023-04-14 14:31:41 -0400

    premake

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+
+-- Import this with a parent PREMAKE project
+
+project "yaml-cpp"
+    location "yaml-cpp"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+
+    systemversion "latest"
+
+    targetdir ("bin/"..output.."/%{prj.name}");
+    objdir ("bin-int/"..output.."/%{prj.name}");
+
+    files {
+        "src/**.cpp",
+        "src/**.h",
+        "include/**.h"
+    }
+
+    includedirs {
+        "include"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        runtime "Release"
+        Optimize "On"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "On"
