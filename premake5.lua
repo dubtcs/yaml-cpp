7e8f54b98e6425cad1483cb490e340b0e63df8be
dubtcs <75275524+dubtcs@users.noreply.github.com>
2023-04-14 14:50:54 -0400

    static lib define

--- premake5.lua
+++ premake5.lua
@@ -16,6 +16,10 @@ project "yaml"
         "include"
     }
 
+    defines {
+        "YAML_CPP_STATIC_DEFINE"
+    }
+
     filter "system:windows"
         systemversion "latest"
         cppdialect "C++20"
